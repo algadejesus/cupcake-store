632450acdea022b97345826767fcb313df4d625c
algadejesus <agladejesus@hotmail.com>
2023-10-11 22:41:06 -0400

    Até a aula: 51. Métodos de Delete

--- Back-end/index.js
+++ Back-end/index.js
@@ -11,7 +11,43 @@ server.get('/first', (req, res) => {
 });
 
 server.get('/query-params', (req, res) => {
-    const {name, idade} = req.query;
+    const {name, sobrenome, idade, email} = req.query;
 
-    return res.json({ressult: `Nome: ${name} e Idade: ${idade}`})
+    return res.json({ressult: `Meu nome é : ${name} ${sobrenome} e tenho : ${idade} de idade, o meu email é ${email}`})
 });
+
+let produtos = [];
+
+server.post('/produtos', (req, res) => {
+    const { id, name, price } = req.body;
+
+    produtos.push({id: id, name: name, price: price});
+
+    res.send({message: 'Success!'});
+})
+
+server.get('/produtos', (req, res) => {
+    res.send({Produto: produtos})
+});
+
+server.put('/produtos', (req, res) => {
+    const {name, price} = req.body;
+    const {oldName} = req.query;
+
+    const index = produtos.findIndex(item => item.name === oldName);
+
+    produtos[index].name = name;
+    produtos[index].price = price;
+
+    res.send({message: 'Success!'});
+});
+
+server.delete('/produtos/:id', (req, res) => {
+    const { id } = req.params;
+
+    const newProdutos = produtos.filter(item => item.id != id);
+
+    produtos = newProdutos;
+
+    res.send({Produtos: produtos});
+})
